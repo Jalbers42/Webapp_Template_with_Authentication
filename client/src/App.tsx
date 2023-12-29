4557a3b2d7efb61100a9768e62e10a6dfd3d17a3
JalbersCode <josephal@hotmail.de>
2023-12-29 08:33:32 +0100

    first

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from "./components/ui/button"
+
+function App() {
+
+  return (
+    <div className="text-3xl">
+      HELLO
+      <Button>WORLD</Button>
+    </div>
+  )
+}
+
+export default App
