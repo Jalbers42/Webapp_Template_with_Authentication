fbce9724d7eb52382879d206c2c1155c76fe06c1
JalbersCode <josephal@hotmail.de>
2023-12-29 14:22:35 +0100

    first

--- client/src/_root/pages/Home.tsx
+++ client/src/_root/pages/Home.tsx
@@ -1,15 +1,31 @@
 import { Button } from "@/components/ui/button"
 import { Textarea } from "@/components/ui/textarea"
-
+import { useState } from "react"
+              
 const Home = () => {
+
+  const [messages, setMessages] = useState(["Hi", "hey"])
+
+  function addMessage() {
+    setMessages([...messages, "NEW"])
+  }
+
+  function clear() {
+    setMessages([])
+  }
+
   return (
     <div className="flex flex-1 items-center justify-center">
-        <div>
-            <div className="grid w-full gap-2">
-                <Textarea placeholder="Type your message here." />
-                <Button>Send message</Button>
-            </div>
+      <div>
+        {messages.map((element, index) => (
+          <div key={index}>{element}</div>
+        ))}
+        <div className="grid w-full gap-2">
+          <Textarea placeholder="Type your message here." />
+          <Button onClick={addMessage}>Send message</Button>
+          <Button onClick={clear}>Clear</Button>
         </div>
+      </div>
     </div>
   )
 }
