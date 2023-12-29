06094f70fcc02cc6299a2a3ff52baab9af82bfd0
JalbersCode <josephal@hotmail.de>
2023-12-29 12:49:51 +0100

    first

--- client/src/_root/pages/Home.tsx
+++ client/src/_root/pages/Home.tsx
@@ -1,6 +1,16 @@
+import { Button } from "@/components/ui/button"
+import { Textarea } from "@/components/ui/textarea"
+
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className="flex flex-1 items-center justify-center">
+        <div>
+            <div className="grid w-full gap-2">
+                <Textarea placeholder="Type your message here." />
+                <Button>Send message</Button>
+            </div>
+        </div>
+    </div>
   )
 }
 
